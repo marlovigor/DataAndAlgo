0e0424ff7caa930b450e8d433f3a825c6522db93
Marlo Jones <marlojones@pop-os.localdomain>
2022-07-08 21:23:16 -0400

    first commit

---
+++ allog.js
@@ -0,0 +1,36 @@
+///recursive function to get all the children of a node///
+
+///counting sheep
+
+function countingdownSheep(num){
+ if(num === 0){
+    console.log( "All sheep jumped over the fence");     
+ }
+    else{
+        let newNum = num - 1 ;
+        countingdownSheep(newNum)
+        console.log(newNum + ": Another sheep jumps over the fence");
+    }
+}
+
+// countingdownSheep(10);
+
+
+powerCalculator = (base, exp) => {
+    if(exp < 0){
+        return "exponent should be >= 0";
+    }
+    else if(exp === 0){
+        return 1;
+    }
+    else{
+        return base * powerCalculator(base, exp - 1);
+    }
+}
+
+console.log(powerCalculator(10, 2));
+
+
+
+
+
